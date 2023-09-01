e54f2e99b47b5a5edd1612eff71c1f4615799a92
jumma <aarifhingorja4@gmail.com>
2023-09-01 19:38:26 +0530

    created invite modal

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { redirectToSignIn } from "@clerk/nextjs";
-
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import ServerSidebar from "@/components/server/server-sidebar";
 
 const ServerIdLayout = async ({
   children,
@@ -38,7 +39,7 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <ServerSidebar />
+        <ServerSidebar serverId={params.serverId}/>
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
