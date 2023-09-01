09a8fe261cc59d4e4894b7ad69a2fa7575987efb
jumma <aarifhingorja4@gmail.com>
2023-09-01 09:47:27 +0530

    added server modal and create server

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface ServerPageProps {}
+
+const ServerPage: FC<ServerPageProps> = ({}) => {
+  return (
+    <div>ServerIdServerPage</div>
+  )
+};
+
+export default ServerPage;
