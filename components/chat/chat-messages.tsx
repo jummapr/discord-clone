8dce9937b5b0f7b7d4e6c143539d3d85cc4dfe3b
jumma <aarifhingorja4@gmail.com>
2023-09-23 06:52:09 +0530

    edit and delete message func

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Member, Message, Profile } from '@prisma/client';
+import {format} from "date-fns"
 import { FC, Fragment } from 'react'
 import { Loader2, ServerCrash } from 'lucide-react';
 
 import ChatWelcome from './chat-welcome';
 import { useChatQuery } from '@/hooks/use-chat-query';
+import ChatItem from './chat-item';
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -79,9 +83,19 @@ const ChatMessages: FC<ChatMessagesProps> = ({
         {data?.pages?.map((group,i) => (
             <Fragment key={i}>
                 {group.items.map((message: MessageWithMemberWithProfile) => (
-                    <div key={message.id}>
-                        {message.content}
-                    </div>
+                    <ChatItem
+                        key={message.id}
+                        id={message.id}
+                        currentMember={member}
+                        member={message.member}
+                        content={message.content}
+                        fileUrl={message.fileUrl}
+                        deleted={message.deleted}
+                        timestamp={format(new Date(message.createdAt),DATE_FORMAT)}
+                        isUpdated={message.updateAt !== message.createdAt}
+                        socketUrl={socketUrl}
+                        socketQuery={socketQuery}
+                    />
                 ))}
             </Fragment>
         ))}
